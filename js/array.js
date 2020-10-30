b5a99ad76bdc31887471d5d0718733e73f12a13b
sarthaksavvy <bitfumes@gmail.com>
2020-10-30 20:45:33 +0530

    17. JavaScript - arrays

---
+++ js/array.js
@@ -0,0 +1,18 @@
+let planets = [
+  "Mercury",
+  "Venus",
+  "Earth",
+  "Mars",
+  "Jupiter",
+  "Saturn",
+  "Uranus",
+  "Naptune",
+];
+// let jovians = planets.slice(4, 8);
+// let terrestrial = planets.slice(0, 4);
+// planets.push("Pluto");
+// planets.pop();
+// planets.unshift("Mercury");
+// planets.shift();
+// planets.splice(planets.indexOf("Jupiter"), 1);
+// console.log(terrestrial.concat(jovians).join(":"));
--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
-firstname = "Sarthak";
-lastname = "Shrivastava";
-console.log(window);
+// null is like filled with air
+// undefined is like vacuume
+
+// evaluate to false -> 0, null, '', undefined
+
+// Numbers
+// NaN is Not a Number
+
+// String
+// define string using -> '', "", ``
+// instance methods
+// 1. concat
+// 2. includes
+// 3. toLowerCase
+// 4. toUpperCase
+// 5. endsWith
+// 6. indexOf
+// 7. trim
+// 8. startsWith
+let statement = `There are "eight planets" in the \`solar system\` and 'earth' is our home`;
+let planet = "   Earth   ";
+console.log(statement.startsWith("T"));
